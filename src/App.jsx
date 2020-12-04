3b5dd43e98e313be4b121300bec33720f17d8cb6
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-12-05 01:55:15 +0530

    Pre-deployment full project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
+import wordsToNumbers from "words-to-numbers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const alanKey = process.env.ALANKEY;
+
+function App() {
+  const classes = useStyles();
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again.");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn.playText("Opening...");
+          } else {
+            return;
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          alt="alan logo"
+          className={classes.alanLogo}
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  );
+}
+
+export default App;
